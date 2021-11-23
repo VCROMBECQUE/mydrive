5539d0db6e3a64834015b2bc62499b6ea4b63779
Valentin <valentin.crombecque@gmail.com>
2021-11-23 11:07:33 +0100

    update

---
+++ html/mydrive.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../public/assets/dist/css/main.min.css">
+    <title>UserDrive</title>
+</head>
+
+<body>
+    <header class="lign-1">
+        <div class="logo">
+            <img class="logo_pic mr-1 ml-1" src="../public/assets/images/logo.svg" alt="logo">
+            <h1 class="heading-2 mr-3">MyDrive</h1>
+        </div>
+        <form action="">
+            <input class="ml-1 mr-1 texting-1" type="text" placeholder="Rechercher sur MyDrive">
+            <button type="submit"><i class="fas fa-search icon-2 mr-1 ml-1"></i></button>
+        </form>
+        <i class="fas fa-user icon-1 signinicon ml-1 mr-3"></i>
+    </header>
+    <main class="userdrive">
+        <div class="userdrive_menu lign-2">
+            <button class="userdrive_menu_btn texting-1 mt-3">Ajouter</button>
+            <button class="userdrive_menu_btn texting-1 mt-3">Partages</button>
+        </div>
+        <div class="userdrive_data">
+
+            <div class="userdrive_data_ariane texting-1 p-3 m-1">
+                <p><i class="fas fa-chevron-right"></i> <span>MyDrive</span> </p>
+            </div>
+
+            <div class="userdrive_data_storage p-3">
+
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/folder.png"
+                        alt="folder">
+                    <p class="userdrive_data_storage_container_name texting-2">secret</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/jpg.png"
+                        alt="jpg">
+                    <p class="userdrive_data_storage_container_name texting-2">jpg</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/png.png"
+                        alt="png">
+                    <p class="userdrive_data_storage_container_name texting-2">png</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/gif.png"
+                        alt="gif">
+                    <p class="userdrive_data_storage_container_name texting-2">gif</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/docx.png"
+                        alt="docx">
+                    <p class="userdrive_data_storage_container_name texting-2">word</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/xlsx.png"
+                        alt="xlsx">
+                    <p class="userdrive_data_storage_container_name texting-2">excel</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/pptx.png"
+                        alt="pptx">
+                    <p class="userdrive_data_storage_container_name texting-2">powerpoint</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/pdf.png"
+                        alt="pdf">
+                    <p class="userdrive_data_storage_container_name texting-2">pdf</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/txt.png"
+                        alt="txt">
+                    <p class="userdrive_data_storage_container_name texting-2">texte</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/mp3.png"
+                        alt="mp3">
+                    <p class="userdrive_data_storage_container_name texting-2">musique la légende des oiseaux</p>
+                </div>
+                <div class="userdrive_data_storage_container p-1">
+                    <img class="userdrive_data_storage_container_icon" src="../public/assets/images/icon/avi.png"
+                        alt="avi">
+                    <p class="userdrive_data_storage_container_name texting-2">vidéo</p>
+                </div>
+
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
